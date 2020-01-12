15b76f8884762eaaf4d3ed4594561ae471020178
Roman <student.rv86@gmail.com>
2020-01-12 17:31:40 +0200

    Add createDB.sql file

---
+++ src/main/resources/sql/createDB.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS accounts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    status VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS skills (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS developers (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    account_id INT NOT NULL
+    FOREIGN KEY (account_id) REFERENCES accounts (id)
+);
+
+CREATE TABLE IF NOT EXISTS skills_in_developers (
+    developer_id INT NOT NULL,
+    skill_id INT NOT NULL,
+    UNIQUE (developer_id, skill_id),
+    FOREIGN KEY (developer_id) REFERENCES developers (id),
+    FOREIGN KEY (skill_id) REFERENCES skills (id)
+);
