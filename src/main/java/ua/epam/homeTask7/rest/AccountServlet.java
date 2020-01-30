9209c3f8f979e573cda13f040189bc67698dc330
Roman <student.rv86@gmail.com>
2020-01-30 22:54:55 +0200

    Add AccountServlet class

--- src/main/java/ua/epam/homeTask7/rest/AccountServlet.java
+++ src/main/java/ua/epam/homeTask7/rest/AccountServlet.java
@@ -12,23 +12,37 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebServlet(name = "AccountServlet", urlPatterns = "/api/v1/accounts")
 public class AccountServlet extends HttpServlet {
+
+    private static Logger logger = Logger.getLogger(AccountServlet.class.getName());
 
     private AccountService accountService = new AccountService();
     private Gson gson = new Gson();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        Account account = new Account(1, "test@test", AccountStatus.ACTIVE);
-        List<Account> accounts = accountService.getAll();
-        String jsonString = gson.toJson(accounts);
-
         PrintWriter out = resp.getWriter();
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
-        out.print(jsonString);
-        out.flush();
+
+        if (req.getParameter("id").equalsIgnoreCase("all")) {
+            List<Account> accounts = accountService.getAll();
+            String jsonAccount = gson.toJson(accounts);
+            out.print(jsonAccount);
+            out.flush();
+        } else if (req.getParameter("id").matches("\\d+")) {
+            long id = Long.parseLong(req.getParameter("id"));
+            Account account = accountService.get(id);
+            String jsonAccount = gson.toJson(account);
+            out.print(jsonAccount);
+            out.flush();
+        } else {
+            resp.sendError(400, "Id is not valid");
+            logger.log(Level.WARNING, "Request with invalid Id");
+        }
     }
 }
